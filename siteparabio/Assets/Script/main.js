bb0bcd9241771f97acf2b7d82a113ad4d33941f2
Pedro Afonso M. Nunes <pedroaf1233@gmail.com>
2021-11-01 22:09:04 +0000

    add conteudo

--- siteparabio/Assets/Script/main.js
+++ siteparabio/Assets/Script/main.js
@@ -81,3 +81,17 @@ function ques1(){
     spnpoi.innerHTML = points+"/10"
     
 }
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
