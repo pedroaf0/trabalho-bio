c8a6f58f7321888f6db5eea8112bc7fc8939ab49
Japagod <80219032+Japagod@users.noreply.github.com>
2021-11-01 16:35:49 -0300

    Add files via upload

--- siteparabio/Assets/Script/main.js
+++ siteparabio/Assets/Script/main.js
@@ -4,47 +4,80 @@ var rot = 0
 var tam = 20;
 var oc = new Boolean(false)
 var lin = document.getElementById("ml3")
+var hide = document.getElementById("coluna")
+hide.style.display = "none"
 function navanim() {
     var elem = document.getElementById("animar");
     var img = document.getElementById("bttimg")
+    var main = document.getElementById("main")
     var id = setInterval(() => {
-        if (tam == 100 && oc == false) {
+        if(oc==false){
+        if (tam == 65 && oc == false) {
             clearInterval(id)
+            hide.style.display = "block"
             oc = true
         } else if (oc == false) {
             tam++
-            rot = rot + 1.15
-            elem.style.width = tam + 'vh'
+            rot = rot + 2.045
+            elem.style.height = tam + 'vh'
             img.style.transform = "rotate" + "(" + rot + "deg" + ")"
-        }
+        }}
+        else
         if (tam == 20 && oc == true) {
             clearInterval(id)
-            oc = false 
+            oc = false
         } else if (oc == true) {
+            hide.style.display = "none"
             tam--
-            rot = rot - 1.15
-            elem.style.width = tam + 'vh'
+            rot = rot - 2.045
+            elem.style.height = tam + 'vh'
             img.style.transform = "rotate" + "(" + rot + "deg" + ")"
+            
         }
-
     }, 7)
 }   
 
                 // Questões
-
-
+var spnpoi = document.getElementById("spnpoi")
 function ques1(){
-    if(document.getElementById("1r4").checked){
-        Swal.fire({
-            icon: 'success',
-            title: 'Obaa',
-            text: 'Você acertou!',
-          })
-    } else {
-        Swal.fire({
-            icon: 'error',
-            title: 'Eita!',
-            text: 'Resposta incorreta',
-          })
+    var points = 0
+    if(document.getElementById("1r").checked){
+        points++
     }
+    if(document.getElementById("2r").checked){
+        points++
+    }
+    if(document.getElementById("3r").checked){
+        points++
+    }
+    if(document.getElementById("4r").checked){
+        points++
+    }
+    if(document.getElementById("5r").checked){
+        points++
+    }
+    if(document.getElementById("6r").checked){
+        points++
+    }
+    if(document.getElementById("7r").checked){
+        points++
+    }
+    if(document.getElementById("8r").checked){
+        points++
+    }
+    if(document.getElementById("9r").checked){
+        points++
+    }
+    if(document.getElementById("10r").checked){
+        points++
+    }
+    if(document.getElementById("11r").checked){
+        points++
+    }
+    if(document.getElementById("12r").checked){
+        points++
+    }
+    points = (10*(points/12)).toFixed(2)
+    spnpoi.innerHTML = points+"/10"
+    
 }
